6ca254732bd513bb8f5e28be5dbce494034ee61e
jeffery017 <jeffery017@gmail.com>
2024-10-16 10:54:53 -0500

    add dockfile

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.include_router(admin_router, prefix='/admin')
 app.include_router(user_router, prefix='/user')
 
 if __name__ == "__main__":  
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
