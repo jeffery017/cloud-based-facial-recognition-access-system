6900a1bb10a622d37942b00dfe2665b0b7ebd013
jeffery017 <jeffery017@gmail.com>
2024-10-30 14:18:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import uvicorn
 from routes.unlock import router as unlock_router
 from routes.admin import router as admin_router
 from routes.user import router as user_router
-from routes.index import router as index_router 
-from qdrant_client.models import Distance, VectorParams 
+from routes.index import router as index_router  
 from dotenv import load_dotenv
 from libs.db import Model
 
@@ -20,12 +19,6 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000) 
     client = Model.getClient()
 
-    #delete user registry collection on start up (fresh database on launch)
-    client.delete_collection(collection_name='RegisteredUsers')
-    #create user registry collection
-    client.create_collection(
-        collection_name='RegisteredUsers',
-        vectors_config=VectorParams(size=4, distance=Distance.DOT)
-    )
+    
 
 
