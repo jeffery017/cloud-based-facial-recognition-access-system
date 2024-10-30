38f6637990e6513b5f9e796c48ecef0acb437d1f
jeffery017 <jeffery017@gmail.com>
2024-10-30 14:18:01 -0500

    Update db.py

--- libs/db.py
+++ libs/db.py
@@ -1,20 +1,27 @@
 
-from qdrant_client import QdrantClient 
 import os
+from qdrant_client import QdrantClient 
+from qdrant_client.models import Distance, VectorParams 
 from dotenv import load_dotenv
-ak = os.getenv("QDRANT_API_KEY")
-end = os.getenv("QDRANT_ENDPOINT")
- 
 
 class Model:
     __client =  QdrantClient(
-        url=end,
-        api_key=ak
+        url=os.getenv("QDRANT_ENDPOINT"),
+        api_key=os.getenv("QDRANT_API_KEY")
     )
 
     @staticmethod
     def getClient():
         return Model.__client
  
+
+client = Model.getClient()
+#delete user registry collection on start up (fresh database on launch)
+client.delete_collection(collection_name='RegisteredUsers')
+#create user registry collection
+client.create_collection(
+    collection_name='RegisteredUsers',
+    vectors_config=VectorParams(size=4, distance=Distance.DOT)
+)
 
 
